1ab20f315a51515a10d9aadc12b74501d00188d8
mrasdfdela <jerryhsu830@gmail.com>
2020-02-27 15:44:12 -0500

    form-based workflow feature completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,57 @@
 class ArticlesController < ApplicationController
+    include ArticlesHelper
+
     def index
         @articles = Article.all
     end
 
     def show
         @article = Article.find(params[:id])
+    end
+
+    def new
+       @article = Article.new 
+    end
+    
+    def create
+        # alternative 1
+        # @article = Article.new
+        # @article.title = params[:article][:title]
+        # @article.body = params[:article][:body]
+        
+        # alternative 2
+        # @article = Article.new(
+        #     title: params[:article][:title],
+        #     body: params[:article][:body]
+        # )
+        
+        #alternative 3; uses 'Strong Parameters' in articles_helper
+        @article = Article.new(article_params)
+        @article.save 
+
+        flash.notice = "Article '#{@article.title}' created!"
+        redirect_to article_path(@article)
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)
+
+        flash.notice = "Article '#{@article.title}' updated!" 
+        
+        redirect_to article_path(@article)
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        # puts @article
+
+        flash.notice = "Article '#{@article.title}' destroyed!" 
+        @article.destroy
+        redirect_to articles_path
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@
 # end
 
 Blogger::Application.routes.draw do
+  root to: 'articles#index'
   resources :articles
 end
