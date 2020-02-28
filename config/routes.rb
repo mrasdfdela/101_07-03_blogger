08a418dcfa0422b52a4f7784ddfc667b98aa9264
mrasdfdela <jerryhsu830@gmail.com>
2020-02-28 16:10:58 -0500

    Tagging feature completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
-# Rails.application.routes.draw do
+Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # resources :articles
-# end
+  
+end
 
 Blogger::Application.routes.draw do
   root to: 'articles#index'
   resources :articles do
     resources :comments
   end
+
+  resources :tags
 end
--- db/migrate/20200227204708_create_comments.rb
+++ db/migrate/20200227204708_create_comments.rb
@@ -5,7 +5,7 @@ class CreateComments < ActiveRecord::Migration[5.2]
       t.text :body
       t.references :article, foreign_key: true
 
-      t.timestamps :timestamp
+      t.timestamps
     end
   end
 end
